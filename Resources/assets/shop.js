df427ab35a3c0b005c749fc7c67cb5d981bba67e
Franz Wilding <franz.wilding@me.com>
2021-08-26 17:28:23 +0200

    Update shop.js

--- Resources/assets/shop.js
+++ Resources/assets/shop.js
@@ -2,23 +2,24 @@ import { Tooltip, Toast, Popover, Alert } from 'bootstrap';
 import inlineForm from './modules/inlineForm';
 import productVariant from './modules/productVariant';
 import checkout from './modules/checkout';
+import feather from 'feather-icons';
+import GLightbox from 'glightbox';
+import addressBookFill from "./modules/addressBookFill";
+import provinceForm from "./modules/provinceForm";
 
 [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]')).map(function (tooltipTriggerEl) {
     return new Tooltip(tooltipTriggerEl);
 });
 
-import feather from 'feather-icons';
 feather.replace();
 
-import GLightbox from 'glightbox';
-import addressBookFill from "./modules/addressBookFill";
-import provinceForm from "./modules/provinceForm";
-GLightbox({
+(() => {
+  GLightbox({
     selector: '.glightbox',
-});
-
-inlineForm();
-productVariant();
-checkout();
-addressBookFill();
-provinceForm();
+  });
+  inlineForm();
+  productVariant();
+  checkout();
+  addressBookFill();
+  provinceForm();
+})();
